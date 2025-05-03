272bd2c13913b9e0a068e7370fcb40e58180bd27
Sultonbek <sultonbek_nazarov@epam.com>
2025-05-03 17:16:10 +0500

    task-3

--- lib/getProductsList.ts
+++ lib/getProductsList.ts
@@ -3,8 +3,14 @@ import { getAllProducts } from "./productService";
 export async function main() {
   const producuts = getAllProducts();
 
-  return {
+  const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    },
     body: JSON.stringify(producuts),
   };
+  return response;
 }
