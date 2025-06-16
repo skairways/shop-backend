2b27f2615bbd70afd3b6320ac11d8aab3cf61856
Sultonbek <sultonbek_nazarov@epam.com>
2025-06-16 18:37:42 +0500

    task-8

--- lib/authorizationServiceStack.ts
+++ lib/authorizationServiceStack.ts
@@ -1,5 +1,4 @@
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cdk from "aws-cdk-lib";
 import * as path from "path";
 import { Construct } from "constructs";
--- lib/lambda/importProductsFile/handler.ts
+++ lib/lambda/importProductsFile/handler.ts
@@ -2,10 +2,8 @@ import { S3 } from "aws-sdk";
 import { SERVER_ERROR } from "../../shared/constant";
 
 export async function importProductsFile(request: any) {
-  console.log("🚀 ~ main ~ request:", request);
   try {
     const fileName = request.name;
-    console.log("🚀 ~ importProductsFile ~ fileName:", fileName);
     const s3 = new S3();
     const bucketName = process.env.IMPORT_BUCKET_NAME;
     const signedUrl = s3.getSignedUrl("getObject", {
@@ -14,10 +12,8 @@ export async function importProductsFile(request: any) {
       Expires: 60,
     });
 
-    console.log("🚀 ~ importProductsFile ~ signedUrl:", signedUrl);
     return signedUrl;
   } catch (error) {
-    console.log("🚀 ~ importProductsFile ~ error:", error);
     return new Error(SERVER_ERROR);
   }
 }
