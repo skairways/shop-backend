ae7962faf2b5237cfae6ab53a0b30d4fa5b4c6ad
Sultonbek <sultonbek_nazarov@epam.com>
2025-06-21 17:32:29 +0500

    task-8 nest app deployed

--- lib/cartServiceStack.ts
+++ lib/cartServiceStack.ts
@@ -4,28 +4,41 @@ import * as path from "path";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { BundlingFileAccess } from "aws-cdk-lib";
 
 export class CartServiceStack extends cdk.Stack {
   api: apigateway.RestApi;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const nestAppLambda = new NodejsFunction(this, "NestAppHandler", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      entry: "../../nodejs-aws-cart-api",
+    const nestAppLambda = new lambda.Function(this, "NestLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "dist/src/serverless-main.handler",
+      code: lambda.Code.fromAsset("dist.zip"),
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 512,
+      environment: {
+        DATABASE_HOST:
+          "rds-cloud-x-database.cetywe2eqcal.us-east-1.rds.amazonaws.com",
+        DATABASE_PORT: "5432",
+        DATABASE_USERNAME: "postgres",
+        DATABASE_PASSWORD: "postgres",
+        DATABASE_NAME: "postgres",
+      },
     });
 
-    this.api = new apigateway.RestApi(this, "NestApi", {
-      restApiName: "Nest Service",
-      description: "This service serves a Nest.js application.",
+    this.api = new apigateway.LambdaRestApi(this, "NestApi", {
+      handler: nestAppLambda,
+      proxy: false,
     });
 
-    const getLambdaIntegration = new apigateway.LambdaIntegration(
-      nestAppLambda
-    );
+    this.api.root.addMethod("ANY");
+
+    const proxyResource = this.api.root.addResource("{proxy+}");
+    proxyResource.addMethod("ANY");
 
     // Output API URL
-    new cdk.CfnOutput(this, "ApiUrl", {
+    new cdk.CfnOutput(this, "Nest app ApiUrl", {
       value: this.api.url,
     });
   }
